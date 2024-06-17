1a53144045f185bc2ca1a6fec2dbb087124f381e
Amrita Gupta <amamrita0818@gmail.com>
2024-06-17 11:44:28 +0530

    githubData Added

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const githubData = {
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 89,
+    "public_gists": 4,
+    "followers": 23900,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2024-05-27T08:52:49Z"
+    }
+
 //yaha get ek request lega, fir hm res.send ki help se response bhezenge 
 //callback ke andar do parameter rahenge, ek request , dusra response
 app.get('/', (req, res) => {
@@ -21,6 +56,11 @@ app.get('/login', (req, res)=>{
 app.get('/youtube', (req, res)=> {
     res.send('<h1>Please subscribe chai aur code</h1>')
 })
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
+
 //agar successfully listen karega to is port par chal jayega
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
